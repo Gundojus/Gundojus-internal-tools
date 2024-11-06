5bab18bdc6513b85dd952aebb821c201e70736e0
Project-Vigil-1602 <project.vigil.1602@gmail.com>
2024-11-07 00:19:30 +0530

    Updates

---
+++ precache-manifest.918f303366377d7362bbbaa0a9d0163a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37149fccb4880615573818f8898782da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d54ba18accd78283c179",
+    "url": "/static/css/2.05cbb0d5.chunk.css"
+  },
+  {
+    "revision": "d01735e6d3c2369a8f1e",
+    "url": "/static/css/main.989288b7.chunk.css"
+  },
+  {
+    "revision": "d54ba18accd78283c179",
+    "url": "/static/js/2.76dd0c5c.chunk.js"
+  },
+  {
+    "revision": "1549e0aff47518b12dfe261410bcbef0",
+    "url": "/static/js/2.76dd0c5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d01735e6d3c2369a8f1e",
+    "url": "/static/js/main.52c0046d.chunk.js"
+  },
+  {
+    "revision": "19ee886c70b19f7e8d31",
+    "url": "/static/js/runtime-main.e28f720f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.905c7bbc7e188e0d06ceffbb0d9180c8.js"
+  "/precache-manifest.918f303366377d7362bbbaa0a9d0163a.js"
 );
 
 self.addEventListener('message', (event) => {
